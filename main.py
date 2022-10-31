0a599a221e5a64a4b32b921ad6c39f80c52c3c55
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-10-31 08:30:36 -0300

    First commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"mensagem": "André Luiz França Batista"})
 
 
 if __name__ == '__main__':
