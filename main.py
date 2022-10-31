245a8c7d844095b1de7817d907bf568651501fb7
zandree <01411769627>
2022-10-31 10:39:26 -0300

    Final version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, render_template, request
 import os
 import predictions
+import torch
 
 app = Flask(__name__, template_folder='templates')
-
 
 @app.route('/')
 def index():
@@ -12,13 +12,25 @@ def index():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        #dados = request.form
-        #y_pred = predictions.predict()
-
-        return jsonify(request.form['comp_petala'])
+        #larg_sepala, comp_sepala, larg_petala, comp_petala
+        #['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+        dados = [
+            float(request.form['comp_sepala']),
+            float(request.form['larg_sepala']),
+            float(request.form['comp_petala']),
+            float(request.form['larg_petala'])
+        ]
+        y_pred = predictions.predict(dados)
+        # print(y_pred)
+        # return jsonify({"result": y_pred})
+        #y_pred = (torch.round(y_pred, decimals=2))
+        return jsonify({
+            "versicolor" : f'{y_pred[0]}',
+            "setosa" : f'{y_pred[1]}',
+            "virginica" : f'{y_pred[2]}'
+            })
     else:
         return jsonify({"mensagem" : "utilize o formulario"})
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
--- predictions.py
+++ predictions.py
@@ -19,12 +19,17 @@ class Net(torch.nn.Module):
     output = self.sigmoid(output)
     return output
 
-modelo = Net()
-modelo.load_state_dict(torch.load('./modelo/iris.dat'))
-modelo.eval()
-
-def predict(larg_sepala, comp_sepala, larg_petala, comp_petala, modelo):
-    dados = [larg_sepala, comp_sepala, larg_petala, comp_petala]
+def predict(dados):
+    #dados = [larg_sepala, comp_sepala, larg_petala, comp_petala]
     entrada = torch.FloatTensor(dados)
-    y_pred = modelo(entrada)
+    # print(entrada)
+    # print(entrada.size())
+    input_size = entrada.size()[0]
+    hidden_size = 10
+    output_size = 3
+    modelo = Net(input_size, hidden_size, output_size)
+    modelo.load_state_dict(torch.load('./modelo/iris.dat'))
+    modelo.eval()
+    y_pred = modelo(entrada) # rodar o modelo com os dados de entrada 
+    y_pred = y_pred.detach().numpy()
     return y_pred
